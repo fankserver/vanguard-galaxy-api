dc78a3c98986f6f7a461d4f757e653b0a53a3550
Florian Kinder <florian.kinder@enthus.de>
2026-09-09 19:58:09 +0200

    fix: raise dungeon example minimum API dependency to 0.3.0

--- examples/DungeonAuthor/Plugin.cs
+++ examples/DungeonAuthor/Plugin.cs
@@ -4,7 +4,7 @@ using VGModAPI;
 namespace DungeonAuthor;
 
 [BepInPlugin("vgmodapi.example.cargo", "Cargo recovery example", "0.1.0")]
-[BepInDependency(ModApi.PluginId, "0.2.6")]
+[BepInDependency(ModApi.PluginId, "0.3.0")]
 public sealed class Plugin : BaseUnityPlugin
 {
     private CargoAuthorSession? _session;
